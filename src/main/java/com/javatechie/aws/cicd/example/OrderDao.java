ed4cf6fe8f12b265fa4600f492b9109222fc5e65
Naman Jain <namanjainyr@Namans-MacBook-Pro-2.local>
2021-09-05 17:43:42 +0530

    Fixed the build issue

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -15,8 +15,8 @@ public class OrderDao {
                 new Order(101, "Mobile", 1, 30000),
                 new Order(58, "Book", 4, 2000),
                 new Order(205, "Laptop", 1, 150000),
-                new Order(809, "headset", 1, 1799)),
-                new Order(111, "Car", 1, 200000)
+                new Order(809, "headset", 1, 1799),
+                new Order(111, "Car", 1, 200000))
                 .collect(Collectors.toList());
     }
 }
